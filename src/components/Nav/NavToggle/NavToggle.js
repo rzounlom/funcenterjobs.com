910af618a12978c1467e9aa7b437811585bb032d
Romaric Zounlome <rzounlom@gmail.com>
2020-05-03 11:08:03 -0400

    updated nav toggle component

--- src/components/Nav/NavToggle/NavToggle.js
+++ src/components/Nav/NavToggle/NavToggle.js
@@ -27,7 +27,7 @@ class NavToggle extends React.Component {
         <ButtonToolbar>
           <Icon name='bars' size='large' onClick={this.toggleDrawer} />
         </ButtonToolbar>
-        <Drawer show={this.state.show} onHide={this.close}>
+        <Drawer show={this.state.show} onHide={this.close} size='small'>
           <Drawer.Body>
             <ul className='drawer-body-menu' onClick={this.close}>
               <Link to='/'>
@@ -57,12 +57,24 @@ class NavToggle extends React.Component {
               >
                 <li className='drawer-body-item'>
                   <div className='menu-item-conainer'>
+                    <Icon className='menu-icon' name='mail' size='large' />
+                    <span className='contact'>Contact</span>
+                  </div>
+                </li>
+              </a>
+              <a
+                href='https://www.google.com/maps/place/1018+W+Edison+Rd,+Mishawaka,+IN+46545/@41.694734,-86.1981197,17z/data=!3m1!4b1!4m5!3m4!1s0x8816cd8b31e76335:0xcdabd144e62763e6!8m2!3d41.694734!4d-86.195931'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <li className='drawer-body-item'>
+                  <div className='menu-item-conainer'>
                     <Icon
-                      className='menu-icon'
-                      name='paper plane'
+                      className='menu-icon location'
+                      name='location arrow'
                       size='large'
                     />
-                    <span className='contact'>Contact</span>
+                    <span className='location'>Location</span>
                   </div>
                 </li>
               </a>
