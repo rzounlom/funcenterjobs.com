ffdab7980461e7269559562d41b85f6c5bbda374
Romaric Zounlome <rzounlom@gmail.com>
2020-05-03 08:50:32 -0400

    Updates

---
+++ precache-manifest.c3f48a97c459bb9f0d4ceab04fa4bd44.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6b21ffcb1632c1a16ce6e4dfefbabf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f966145ed3c48d59854d",
+    "url": "/static/css/2.cde7393f.chunk.css"
+  },
+  {
+    "revision": "be0802114020b7265001",
+    "url": "/static/css/main.1746406c.chunk.css"
+  },
+  {
+    "revision": "f966145ed3c48d59854d",
+    "url": "/static/js/2.f2088269.chunk.js"
+  },
+  {
+    "revision": "beef77b61353d21277afd681aefe48c1",
+    "url": "/static/js/2.f2088269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be0802114020b7265001",
+    "url": "/static/js/main.05cb5288.chunk.js"
+  },
+  {
+    "revision": "bb733ae28f406c98e67b",
+    "url": "/static/js/runtime-main.30d86e35.js"
+  },
+  {
+    "revision": "c66140dbeb11650503fe34216594004f",
+    "url": "/static/media/innovation.c66140db.jpg"
+  },
+  {
+    "revision": "1c2da67e969c01da18bbb157cb0f1942",
+    "url": "/static/media/landing.1c2da67e.jpeg"
+  },
+  {
+    "revision": "ded9ad59a42bf7fb713323acce7ed56a",
+    "url": "/static/media/people.ded9ad59.jpg"
+  }
+]);
